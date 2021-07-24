8e93cf58419df7184882d5d960f8c1eade4ff54f
Valentinpodolnikov <velman999@mail.ru>
2021-07-24 23:45:13 +0300

    final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,15 +2,62 @@ package com.company;
 
 import java.util.Scanner;
 
+
 public class Main {
+    public static void currentState(int state) {
+        if (state == 1) {
+            System.out.println("Лампочка включена");
+        } else if (state == 2) {
+            System.out.println("Лампочка выключена");
+        } else {
+            System.out.println("Лампочка выключена");
+        }
+    }
 
     public static void main(String[] args) {
-	Bulb bulb1 = new Bulb();
-        Bulb bulb2 = new Bulb(1, Bulb.Color.red, 3);
+        Scanner input = new Scanner(System.in);
 
+        Bulb bulb1 = new Bulb(100, Bulb.Color.white, 3);
 
         System.out.println(bulb1.getInString());
-        System.out.println("==========");
-        System.out.println(bulb2.getInString());
+
+        int action = -1;
+        boolean isRun = true;
+
+        while (isRun) {
+            System.out.println("==========");
+            System.out.println("Что сделать с лампочкой?");
+            System.out.println("1. Выключить/включить");
+            System.out.println("2. Выбрать процент яркости");
+            System.out.println("0. Ничего");
+
+            System.out.print("Введите номер пункта меню: ");
+
+            action = input.nextInt();
+
+            switch (action) {
+                case 1: {
+                    System.out.println("Чтобы включить лампочку нажмите 1, чтобы выключить нажмите 2");
+                    int state = input.nextInt();
+                    currentState(state);
+                }
+                break;
+
+                case 2: {
+                    int brightness = input.nextInt();
+                    System.out.println("Яркость лампочки: "+brightness+"%");
+                }
+                break;
+                case 0: {
+                    isRun = false;
+                }
+                break;
+
+                default: {
+                    System.out.println("Ошибка. Такой команды не существует");
+                }
+                break;
+            }
+        }
     }
 }
