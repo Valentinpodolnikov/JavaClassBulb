59dd672e6969e2904790d2c1f214e1c3ba95e088
Valentinpodolnikov <velman999@mail.ru>
2021-07-28 14:42:33 +0300

    final3

--- src/com/company/Bulb.java
+++ src/com/company/Bulb.java
@@ -7,7 +7,8 @@ public class Bulb {
         red("красный"),
         green("зелёный"),
         blue("голубой"),
-        white("белый");
+        black("чёрный"),
+        yellow("жёлтый");
 
         private final String value;
 
@@ -19,50 +20,129 @@ public class Bulb {
             return value;
         }
     }
-
     //endregion
+
+    public enum State {
+        on("включена"),
+        off("выключена");
+
+        private final String value;
+
+        State(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+
+
+
 
     //region Variables
     //переменные
 
+    private int id;
     private int power;
-    private Color color;
+    private int brightness;
+    private String brandName;
     private int lifeTime;
+    private Color color;
+    private State state;
 
     //endregion
 
     //region Constructors
     //Конструкторы. задаем значения переменных
 
-    public Bulb(){
-        power = 100;
-        color = Color.white;
-        lifeTime = 2;
-    }
 
-
-
-    public Bulb(int power, Color color, int lifeTime) {
+    public Bulb(int id, int power, int brightness, String brandName, int lifeTime, Color color, State state) {
+        this.id = id;
         this.power = power;
+        this.brightness = brightness;
+        this.brandName = brandName;
         this.color = color;
         this.lifeTime = lifeTime;
+        this.state = state;
+
     }
 
     //конструктор копирования
-    public Bulb(Bulb bulb) {
+    public Bulb(int id, Bulb bulb) {
+        this.id = id;
         power = bulb.power;
         color = bulb.color;
         lifeTime = bulb.lifeTime;
+        brightness = bulb.brightness;
+        brandName = bulb.brandName;
+        state = bulb.state;
     }
 
     //endregion
 
-    public String getInString(){
+    //region Setters
+
+    public void setPower(int power) throws Exception {
+        if (power <= 0 || power > 1500) {
+            throw new Exception("Мощность должна быть не меньше 0 и не более 1500");
+        }
+        this.power = power;
+    }
+
+    public void setBrightness(int brightness) throws Exception {
+        if (brightness <= 0 || brightness > 100) {
+            throw new Exception("Яркость не должна быть меньше 0 и не более 100%");
+        }
+        this.brightness = brightness;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+    //endregion
+
+    //region Getters
+
+    public int getPower() {
+        return power;
+    }
+
+    public int getBrightness() {
+        return brightness;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    //endregion
+
+
+    public String getInString() {
         String output = "";
 
-        output += "мощность: " +power+ "(Вт)\n";
-        output += "долговечность: " +lifeTime+ "(г)\n";
-        output += "цвет: " +color.getValue();
+        output += "ИД: " + id + "\n";
+        output += "мощность: " + power + "(Вт)\n";
+        output += "яркость: " + brightness + "(Вт)\n";
+        output += "марка: " + brandName + "\n";
+        output += "долговечность: " + lifeTime + "(г)\n";
+        output += "цвет: " + color.getValue() + "\n";
+        output += "состояние лампочки: " + state.getValue() + "\n";
+
 
         return output;
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,65 +4,107 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void currentState(int state) {
-        if (state == 1) {
-            System.out.println("Лампочка включена");
-        } else if (state == 2) {
-            System.out.println("Лампочка выключена");
-        } else {
-            System.out.println("Лампочка выключена");
-        }
-    }
-
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        Bulb bulb1 = new Bulb(100, Bulb.Color.white, 3);
-
-        System.out.println(bulb1.getInString());
+        BulbManager bulbManager = new BulbManager();
 
         int action = -1;
         boolean isRun = true;
-        int brightness = 100;
-        int state = 2;
+
 
         while (isRun) {
-            System.out.println("==========");
-            System.out.println("Что сделать с лампочкой?");
-            System.out.println("1. Выключить/включить");
-            System.out.println("2. Выбрать процент яркости");
-            System.out.println("0. Ничего");
 
-            System.out.print("Введите номер пункта меню: ");
+            bulbManager.printBulbs();
+            ConsoleHelper.PrintMessage("Меню");
+            ConsoleHelper.PrintMessage("1. Добавить лампочку");
+            ConsoleHelper.PrintMessage("2. Удалить лампочку");
+            ConsoleHelper.PrintMessage("3. Изменить мощность лампочки по ИД");
+            ConsoleHelper.PrintMessage("4. Изменить яркость лампочки по ИД");
+            ConsoleHelper.PrintMessage("5. Изменить цвет лампочки по ИД");
+            ConsoleHelper.PrintMessage("6. Включить/выключить лампочку");
+            ConsoleHelper.PrintMessage("0. Выход");
 
-            action = input.nextInt();
+
+            action = ConsoleHelper.InputInt("Введите номер пункта меню: ", 0, 6);
 
             switch (action) {
                 case 1: {
-                    System.out.println("Чтобы включить лампочку нажмите 1, чтобы выключить нажмите 2");
-                    state = input.nextInt();
-                    currentState(state);
-                    System.out.println("Яркость лампочки: "+brightness+"%");
+                    int power = ConsoleHelper.InputInt("Введите мощность лампочки (Вт): ");
+                    int brightness = ConsoleHelper.InputInt("Введите яркость лампочки: ");
+                    String brandName = ConsoleHelper.InputString("Введите марку лампочки: ");
+                    int lifeTime = ConsoleHelper.InputInt("Введите долговечность лампочки (г): ");
+                    int colorIndex = ConsoleHelper.InputInt("Введите индекс цвета(0-красный, 1-зелёный, 2-голубой, 3-чёрный, 4-жёлтый): ", 0, 4);
+                    Bulb.Color color = Bulb.Color.values()[colorIndex];
+                    int stateIndex = ConsoleHelper.InputInt("Выберите статус лампочки. 1-включена, 2-выключена: ");
+                    Bulb.State state = Bulb.State.values()[stateIndex];
+
+                    bulbManager.addBulb(power, brightness, brandName, lifeTime, color, state);
                 }
                 break;
 
                 case 2: {
-                    System.out.println("Введите необходимую яркость в %: ");
-                    brightness = input.nextInt();
-                    currentState(state);
-                    System.out.println("Яркость лампочки: "+brightness+"%");
+                    try {
+                        int id = ConsoleHelper.InputInt("Введите ИД лампочки для удаления: ");
+                        bulbManager.deleteBulb(id);
+                    } catch (Exception e) {
+                        ConsoleHelper.PrintMessage(e.getMessage());
+                    }
+
+                }
+
+                break;
+
+                case 3: {
+                    try {
+                        int id = ConsoleHelper.InputInt("Введите ИД лампочки для изменения мощности: ");
+                        int power = ConsoleHelper.InputInt("Введите мощность: ");
+                        bulbManager.getByIdBulb(id).setPower(power);
+                    } catch (Exception e) {
+                        ConsoleHelper.PrintMessage(e.getMessage());
+                    }
                 }
                 break;
+
+                case 4: {
+                    try {
+                        int id = ConsoleHelper.InputInt("Введите ИД лампочки для изменения яркости: ");
+                        int brightness = ConsoleHelper.InputInt("Введите яркость лампочки(%): ");
+                        bulbManager.getByIdBulb(id).setBrightness(brightness);
+                    } catch (Exception e) {
+                        ConsoleHelper.PrintMessage(e.getMessage());
+                    }
+                }
+                break;
+
+                case 5: {
+                    try {
+                        int id = ConsoleHelper.InputInt("Введите ИД лампочки для изменения цвета: ");
+                        int colorIndex = ConsoleHelper.InputInt("Выберите новый цвет: ");
+                        bulbManager.getByIdBulb(id).setColor(Bulb.Color.values()[colorIndex]);
+                    } catch (Exception e) {
+                        ConsoleHelper.PrintMessage(e.getMessage());
+                    }
+                }
+                break;
+
+                case 6: {
+                    try {
+                        int id = ConsoleHelper.InputInt("Введите ИД лампочки, которую хотите включить/выключить: ");
+                        int stateIndex = ConsoleHelper.InputInt("1-включить, 2-выключить: ");
+                        bulbManager.getByIdBulb(id).setState(Bulb.State.values()[stateIndex]);
+
+                    } catch (Exception e) {
+                        ConsoleHelper.PrintMessage(e.getMessage());
+                    }
+                }
+                break;
+
                 case 0: {
                     isRun = false;
                 }
                 break;
-
-                default: {
-                    System.out.println("Ошибка. Такой команды не существует");
-                }
-                break;
             }
+
         }
     }
 }
+
